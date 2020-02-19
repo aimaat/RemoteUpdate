b02218e22496551acca7bfb2333eec4cf56558d3
aimaat <info@aima.at>
2020-02-19 23:27:34 +0100

    Add Global class

---
+++ RemoteUpdate/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteUpdate
+{
+    static class Global
+    {
+        public static System.Data.DataTable TableSettings = new System.Data.DataTable("Settings");
+        public static System.Data.DataTable TableRuntime = new System.Data.DataTable("Runtime");
+        public static string Teststring = "TEST";
+    }
+}
