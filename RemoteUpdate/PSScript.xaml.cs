d60207a71fe19fc893001e11456804f8c33b08cf
aimaat <info@aima.at>
2020-04-29 00:39:11 +0200

    Script Save Basics

--- RemoteUpdate/PSScript.xaml.cs
+++ RemoteUpdate/PSScript.xaml.cs
@@ -19,11 +19,16 @@ namespace RemoteUpdate
     /// </summary>
     public partial class PSScript : Window
     {
+        System.Data.DataTable TableScripts = new System.Data.DataTable("Scripts");
         public PSScript()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
             Owner = Application.Current.MainWindow;
+            //TableScripts.Columns.Add("ID", typeof(int),);
+            TableScripts.Columns.Add("Name");
+            TableScripts.Columns.Add("Script");
+            //ComboBoxScript.ItemsSource = TableScripts.DefaultView;
         }
         private void ButtonCancel(object sender, RoutedEventArgs e)
         {
@@ -34,6 +39,34 @@ namespace RemoteUpdate
         {
             this.DialogResult = true;
             this.Close();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            string strScript = TextBoxScript.Text.Trim();
+            string strName;
+            int iID = ComboBoxScript.SelectedIndex;
+            if(iID == -1)
+            {
+                strName = ComboBoxScript.Text;
+            } else
+            {
+                strName = ComboBoxScript.SelectedItem.ToString().Trim();
+            }
+            if (strScript.Length > 1 && strName.Length > 1)
+            {
+                if(iID == -1)
+                {
+                    System.Data.DataRow tmpRow = TableScripts.NewRow();
+                    tmpRow["Name"] = strName;
+                    tmpRow["Script"] = strScript;
+                    TableScripts.Rows.Add(tmpRow);
+                } else
+                {
+                    TableScripts.Rows[iID]["Name"] = strName;
+                    TableScripts.Rows[iID]["Script"] = strScript;
+                }
+            }
         }
     }
 }
